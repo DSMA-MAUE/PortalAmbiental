101568013b2db9961cb796a89255277e3d9ec9b9
Max9th <154016869+Max9th@users.noreply.github.com>
2025-04-10 22:19:45 -0300

    Formatting fixes, changed hero image

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function toggleIconVisibility() {
   const icon = document.querySelector(".hero-icon");
   const container = document.querySelector(".hero-content");
-  if (!icon || !container) return;
+  if (!icon || !container) return; // Returns if there isn't an icon or container
 
   if (window.innerWidth < 900) {
     icon.style.display = "none";
@@ -12,5 +12,21 @@ function toggleIconVisibility() {
   }
 }
 
+function toggleOtherLogoTextVisibility() {
+  const container = document.querySelector(".other-logo-text");
+  const parent_container = document.querySelector(".logo-text");
+  if (!container || !parent_container) return; // Returns if there isn't an icon or container
+
+  if (window.innerWidth < 768) {
+    container.style.display = "none";
+    //container.classList.add("centered");
+  } else {
+    container.style.display = "block";
+    // container.classList.remove("centered");
+  }
+}
+
 window.addEventListener("load", toggleIconVisibility);
 window.addEventListener("resize", toggleIconVisibility);
+window.addEventListener("load", toggleOtherLogoTextVisibility);
+window.addEventListener("resize", toggleOtherLogoTextVisibility);
